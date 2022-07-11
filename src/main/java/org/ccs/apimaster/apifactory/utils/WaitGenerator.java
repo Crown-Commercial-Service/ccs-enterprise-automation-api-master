d527a773b939df5f4b0907ccbec49b8073abacef
mboban2021 <mibin.boban@crowncommercial.gov.uk>
2022-07-11 14:19:24 +0100

    Wait generator

---
+++ src/main/java/org/ccs/apimaster/apifactory/utils/WaitGenerator.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2021 Crown Commercial Service. All rights reserved This Test Automation Solution is the confidential
+ * and proprietary information of Crown Commercial Service. You shall not disclose such confidential information and
+ * shall use it only in accordance with the terms of the license agreement you entered into with Crown Commercial Service.
+ * Mentor: Anne Vaudrey-McVey, CCS Enterprise Test Manager
+ * Author: Mibin Boban, CCS Senior QAT Analyst
+ * Development period: Nov-Dec, 2021
+ */
+package org.ccs.apimaster.apifactory.utils;
+
+public class WaitGenerator {
+
+    public void milliSeconds(int milliSec) throws InterruptedException {
+        Thread.sleep(milliSec);
+    }
+
+    public void seconds(int seconds) throws InterruptedException {
+        Thread.sleep(seconds*1000);
+    }
+
+    public void minutes(int min) throws InterruptedException {
+        Thread.sleep(min*60*1000);
+    }
+
+}
+
+
